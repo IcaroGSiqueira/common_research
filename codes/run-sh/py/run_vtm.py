65ff13805f5883d654c23c80d934825bea0b91c5
IcaroGSiqueira <icarogs1998@gmail.com>
2020-10-13 19:45:32 -0300

    vtm_script

--- codes/run-sh/py/run_vtm.py
+++ codes/run-sh/py/run_vtm.py
@@ -12,7 +12,7 @@ confs = ["encoder_randomaccess_vtm.cfg"]
 
 minqs = [8,16,32]
 
-OPT = 0 # optimizacoes ligadas = 1
+OPT = 1 # optimizacoes ligadas = 1
 gprof = 0
 
 threads = 4 # numero de processos em parelelo
@@ -37,7 +37,9 @@ threads = 4 # numero de processos em parelelo
 
 yuvsvvc = ["MarketPlace_1920x1080_60fps_10bit_420.yuv","RitualDance_1920x1080_60fps_10bit_420.yuv","ArenaOfValor_1920x1080_60fps_8bit_420.yuv","BasketballDrillText_832x480_50fps_8bit_420.yuv","SlideShow_1280x720_20fps_8bit_420.yuv","Campfire_3840x2160_30fps_10bit_420.yuv"]#,"KristenAndSara_1280x720_60fps_8bit_420.yuv","FoodMarket4_3840x2160_60fps_10bit_420.yuv","Tango2_3840x2160_60fps_10bit_420.yuv","ParkRunning3_3840x2160_50fps_10bit_420.yuv","CatRobot_3840x2160_60fps_10bit_420.yuv","DaylightRoad2_3840x2160_60fps_10bit_420.yuv"]	#VVC
 
-yuvshevc = ["RaceHorses_832x480_30fps_8bit_420","BasketballDrill_832x480_50.yuv","BQMall_832x480_60.yuv","PartyScene_832x480_50.yuv","BlowingBubbles_416x240_50.yuv","BQSquare_416x240_60.yuv","BasketballPass_416x240_50.yuv","RaceHorses_416x240_30fps_8bit_420.yuv","BasketballDrive_1920x1080_50.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv"]#,"FourPeople_1280x720_60.yuv","Johnny_1280x720_60.yuv","SlideEditing_1280x720_30.yuv"]	#VVC_HEVC
+yuvshevc = ["RaceHorses_832x480_30.yuv","BasketballDrill_832x480_50.yuv","BQMall_832x480_60.yuv","PartyScene_832x480_50.yuv","BlowingBubbles_416x240_50.yuv","BQSquare_416x240_60.yuv","BasketballPass_416x240_50.yuv","RaceHorses_416x240_30.yuv","BasketballDrive_1920x1080_50.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv"]#,"FourPeople_1280x720_60.yuv","Johnny_1280x720_60.yuv","SlideEditing_1280x720_30.yuv"]	#VVC_HEVC
+
+yuvs = ["RaceHorses_832x480_30.yuv","BasketballDrill_832x480_50.yuv","BQMall_832x480_60.yuv","PartyScene_832x480_50.yuv","BlowingBubbles_416x240_50.yuv","BQSquare_416x240_60.yuv","BasketballPass_416x240_50.yuv","RaceHorses_416x240_30.yuv","BasketballDrive_1920x1080_50.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv","MarketPlace_1920x1080_60fps_10bit_420.yuv","RitualDance_1920x1080_60fps_10bit_420.yuv","ArenaOfValor_1920x1080_60fps_8bit_420.yuv","BasketballDrillText_832x480_50fps_8bit_420.yuv","SlideShow_1280x720_20fps_8bit_420.yuv","Campfire_3840x2160_30fps_10bit_420.yuv"]#,"FourPeople_1280x720_60.yuv","Johnny_1280x720_60.yuv","KristenAndSara_1280x720_60fps_8bit_420.yuv","FoodMarket4_3840x2160_60fps_10bit_420.yuv","Tango2_3840x2160_60fps_10bit_420.yuv","SlideEditing_1280x720_30.yuv","ParkRunning3_3840x2160_50fps_10bit_420.yuv","CatRobot_3840x2160_60fps_10bit_420.yuv","DaylightRoad2_3840x2160_60fps_10bit_420.yuv"]	#VVC
 
 #yuvs=["BlowingBubbles_416x240_50.yuv"] #test
 
@@ -47,7 +49,8 @@ yuvshevc = ["RaceHorses_832x480_30fps_8bit_420","BasketballDrill_832x480_50.yuv"
 #encpath = "vtm9.1"
 #confpath = "%s/cfg"%encpath
 #binpath = "%s/bin"%encpath #partindo da homepath
-#shpath = "git_repo/common_research/codes/run-sh/vtm"
+#shpath = "%s/Documents/pesquisa/common_research/codes/run-sh/vtm"%homepath
+
 
 homepath = "/home/grellert"
 
@@ -88,93 +91,94 @@ else:
 
 #file = open("/home/icaro/git_repo/common_research/codes/run-sh/vtm/%s"%filename,"w")
 
-file = open("%s/%s/%s"%(homepath,shpath,filename),"w")
+file = open("%s/%s"%(shpath,filename),"w")
 
 try:
-	os.system("mkdir %s/%s/"%(homepath,outpath))
+	os.system("mkdir %s/"%(outpath))
 except:
 	pass
 try:
-	os.system("mkdir %s/%s/bin"%(homepath,outpath))
+	os.system("mkdir %s/bin"%(outpath))
 except:
 	pass
 try:
-	os.system("mkdir %s/%s/out"%(homepath,outpath))
+	os.system("mkdir %s/out"%(outpath))
 except:
 	pass
 
 if gprof == 1:
 	try:
-		os.system("mkdir %s/%s/gprof"%(homepath,outpath))
+		os.system("mkdir %s/gprof"%(outpath))
 	except:
 		pass
 	try:
-		os.system("mkdir %s/%s/gmon"%(homepath,outpath))
+		os.system("mkdir %s/gmon"%(outpath))
 	except:
 		pass
 
-for yuv in yuvs:
-	for qp in qps:
-		#for minq in minqs:
+for conf in confs:
+	for yuv in yuvs:
+		for qp in qps:
+			#for minq in minqs:
 
-		print(yuv)
-		try:
-			vid,pix,fr,b,dummy = yuv.split("_")
-			b = b.strip("bit")
-			fr = fr.strip("fps")
-			nome = vid+"_"+pix+"_"+fr+"fps"+"_"+b+"bit"
-		except:
-			vid,pix,fr = yuv.split("_")
-			b = "8"
-			fr = fr.strip(".yuv")
-			nome = vid+"_"+pix+"_"+fr+"fps"+"_"+b+"bit"					
-		w,h = pix.split("x")
-
-		if conf == "encoder_randomaccess_vtm.cfg":
-			info = "%sqp_%sfframes_RA_"%(qp,f) + inf
-		elif conf == "encoder_lowdelay_vtm.cfg":
-			info = "%sqp_%sfframes_LD_"%(qp,f) + inf
-		elif conf == "encoder_intra_vtm.cfg":
-			info = "%sqp_%sfframes_IO_"%(qp,f) + inf
-		else:
-			info = "%sqp_%sfframes_"%(qp,f) + inf
-
-		#linha = "%s/%s/%s -c %s/%s/%s -i \"%s/%s\" -fr %s -wdt %s -hgt %s -q %s -f %s --MinQTLumaISlice=%s --MinQTChromaISliceInChromaSamples=%s --MinQTNonISlice=%s --InputBitDepth=%s --SIMD=%s -b \"%s/%s/bin/%s_%s.bin\" "%(homepath,binpath,bina,homepath,confpath,conf,yuvpath,yuv,fr,w,h,qp,f,minq,int(minq/2),minq,b,simd,homepath,outpath,nome,info) # Linha de configuracao da codificacao
-
-		try:
-			linha = "%s/%s -c %s/%s -i \"%s/%s\" -fr %s -wdt %s -hgt %s -q %s -f %s --InputBitDepth=%s --SIMD=%s -b \"%s/bin/%s_%s.bin\" "%(binpath,bina,confpath,conf,yuvvvc,yuv,fr,w,h,qp,f,b,simd,outpath,nome,info) # Linha de configuracao da codificacao
-		except:
-			linha = "%s/%s -c %s/%s -i \"%s/%s\" -fr %s -wdt %s -hgt %s -q %s -f %s --InputBitDepth=%s --SIMD=%s -b \"%s/bin/%s_%s.bin\" "%(binpath,bina,confpath,conf,yuvhevc,yuv,fr,w,h,qp,f,b,simd,outpath,nome,info) # Linha de configuracao da codificacao
-
-		linha1 = "> %s/%s/out/%s_%s.txt"%(homepath,outpath,nome,info) # linha da saida da codificacao
-
-		# linhas do profiling
-		linha2 = "mv gmon.out %s/gmon/gmon_%s_%s.out"%(outpath,nome,info)
-
-		linha3 = "gprof %s/%s %s/gmon/gmon_%s_%s.out > %s/gprof/%s_%s.txt"%(binpath,bina,outpath,nome,info,outpath,nome,info)
-
-		linha4 =  "echo \"%s_%s DONE!\""%(nome,info)
-
-		#VERIFICAR SOBRESCRICAO
-		if gprof == 1:
+			print(yuv)
 			try:
-				test = open("%s/%s"%(shpath,filename),"r")
-				tlines = test.readlines()
-				tline = tlines[0]
-				if linha not in tline:
-					file.write(linha + linha1 + " && " + linha4 + "\n")
+				vid,pix,fr,b,dummy = yuv.split("_")
+				b = b.strip("bit")
+				fr = fr.strip("fps")
+				nome = vid+"_"+pix+"_"+fr+"fps"+"_"+b+"bit"
 			except:
-				file.write(linha + linha1 + " && " + linha2 + " && " + linha3 + " && " + linha4 + "\n")
+				vid,pix,fr = yuv.split("_")
+				b = "8"
+				fr = fr.strip(".yuv")
+				nome = vid+"_"+pix+"_"+fr+"fps"+"_"+b+"bit"					
+			w,h = pix.split("x")
 
-		else:	
-			try:
-				test = open("%s/%s_%s.txt"%(outpath,nome,info),"r")
-				tlines = test.readlines()
-				tline = tlines[-1]
-				if "Total Time" not in tline:
+			if conf == "encoder_randomaccess_vtm.cfg":
+				info = "%sqp_%sfframes_RA_"%(qp,f) + inf
+			elif conf == "encoder_lowdelay_vtm.cfg":
+				info = "%sqp_%sfframes_LD_"%(qp,f) + inf
+			elif conf == "encoder_intra_vtm.cfg":
+				info = "%sqp_%sfframes_IO_"%(qp,f) + inf
+			else:
+				info = "%sqp_%sfframes_"%(qp,f) + inf
+
+			#linha = "%s/%s/%s -c %s/%s/%s -i \"%s/%s\" -fr %s -wdt %s -hgt %s -q %s -f %s --MinQTLumaISlice=%s --MinQTChromaISliceInChromaSamples=%s --MinQTNonISlice=%s --InputBitDepth=%s --SIMD=%s -b \"%s/%s/bin/%s_%s.bin\" "%(homepath,binpath,bina,homepath,confpath,conf,yuvpath,yuv,fr,w,h,qp,f,minq,int(minq/2),minq,b,simd,homepath,outpath,nome,info) # Linha de configuracao da codificacao
+
+			if yuv in ["MarketPlace_1920x1080_60fps_10bit_420.yuv","RitualDance_1920x1080_60fps_10bit_420.yuv","ArenaOfValor_1920x1080_60fps_8bit_420.yuv","BasketballDrillText_832x480_50fps_8bit_420.yuv","SlideShow_1280x720_20fps_8bit_420.yuv","Campfire_3840x2160_30fps_10bit_420.yuv"]:
+				linha = "%s/%s -c %s/%s -i \"%s/%s\" -fr %s -wdt %s -hgt %s -q %s -f %s --InputBitDepth=%s --SIMD=%s -b \"%s/bin/%s_%s.bin\" "%(binpath,bina,confpath,conf,yuvvvc,yuv,fr,w,h,qp,f,b,simd,outpath,nome,info) # Linha de configuracao da codificacao
+			elif yuv in ["RaceHorses_832x480_30.yuv","BasketballDrill_832x480_50.yuv","BQMall_832x480_60.yuv","PartyScene_832x480_50.yuv","BlowingBubbles_416x240_50.yuv","BQSquare_416x240_60.yuv","BasketballPass_416x240_50.yuv","RaceHorses_416x240_30.yuv","BasketballDrive_1920x1080_50.yuv","BQTerrace_1920x1080_60.yuv","Cactus_1920x1080_50.yuv"]:
+				linha = "%s/%s -c %s/%s -i \"%s/%s\" -fr %s -wdt %s -hgt %s -q %s -f %s --InputBitDepth=%s --SIMD=%s -b \"%s/bin/%s_%s.bin\" "%(binpath,bina,confpath,conf,yuvhevc,yuv,fr,w,h,qp,f,b,simd,outpath,nome,info) # Linha de configuracao da codificacao
+
+			linha1 = "> %s/out/%s_%s.txt"%(outpath,nome,info) # linha da saida da codificacao
+
+			# linhas do profiling
+			linha2 = "mv gmon.out %s/gmon/gmon_%s_%s.out"%(outpath,nome,info)
+
+			linha3 = "gprof %s/%s %s/gmon/gmon_%s_%s.out > %s/gprof/%s_%s.txt"%(binpath,bina,outpath,nome,info,outpath,nome,info)
+
+			linha4 =  "echo \"%s_%s DONE!\""%(nome,info)
+
+			#VERIFICAR SOBRESCRICAO
+			if gprof == 1:
+				try:
+					test = open("%s/%s"%(shpath,filename),"r")
+					tlines = test.readlines()
+					tline = tlines[0]
+					if linha not in tline:
+						file.write(linha + linha1 + " && " + linha4 + "\n")
+				except:
+					file.write(linha + linha1 + " && " + linha2 + " && " + linha3 + " && " + linha4 + "\n")
+
+			else:	
+				try:
+					test = open("%s/%s_%s.txt"%(outpath,nome,info),"r")
+					tlines = test.readlines()
+					tline = tlines[-1]
+					if "Total Time" not in tline:
+						file.write(linha + linha1 + " && " + linha4 + "\n")
+				except:
 					file.write(linha + linha1 + " && " + linha4 + "\n")
-			except:
-				file.write(linha + linha1 + " && " + linha4 + "\n")
 
 
 i=0
